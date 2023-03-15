6829519e0941654322aab39e6dddea071823a2c7
Thomas Alrek <thomas@alrek.no>
2023-03-15 15:33:38 +0100

    Refactor internals

--- src/Column.php
+++ src/Column.php
@@ -4,8 +4,6 @@ namespace Netflex\Database\DBAL;
 
 use Illuminate\Support\Str;
 
-use Netflex\API\Client;
-
 use Doctrine\DBAL\Exception;
 
 use Doctrine\DBAL\Schema\Column as DoctrineColumn;
@@ -15,6 +13,7 @@ use Doctrine\DBAL\Types\IntegerType;
 use Doctrine\DBAL\Types\SimpleArrayType;
 use Doctrine\DBAL\Types\TextType;
 use Doctrine\DBAL\Types\Type;
+use Netflex\Database\Driver\Connection;
 
 final class Column
 {
@@ -25,67 +24,14 @@ final class Column
         $this->column = $column;
     }
 
-    protected const RESERVED_FIELDS = [
-        'id' => [
-            'type' => 'integer',
-            'notnull' => true,
-            'autoincrement' => true,
-            'comment' => 'Primary key'
-        ],
-        'name' => [
-            'type' => 'text',
-            'notnull' => true,
-            'comment' => 'Name of the entry'
-        ],
-        'directory_id' => [
-            'type' => 'integer',
-            'notnull' => true,
-            'comment' => 'The directory this entry belongs to'
-        ],
-        'revision' => [
-            'type' => 'integer',
-            'autoincrement' => true,
-            'notnull' => true,
-            'comment' => 'Current revision'
-        ],
-        'published' => [
-            'type' => 'boolean',
-            'notnull' => true,
-            'comment' => 'Whether or not this entry is published'
-        ],
-        'userid' => [
-            'type' => 'integer',
-        ],
-        'use_time' => [
-            'type' => 'boolean',
-            'notnull' => true,
-            'comment' => 'Whether or not this entry uses time based publishing'
-        ],
-        'start' => [
-            'type' => 'datetime',
-            'notnull' => false,
-            'comment' => 'From when this entry should be published'
-        ],
-        'stop' => [
-            'type' => 'datetime',
-            'notnull' => false,
-            'comment' => 'When this entry should be unpublished'
-        ],
-        'public' => [
-            'type' => 'boolean',
-            'notnull' => false,
-            'comment' => 'Not used'
-        ]
-    ];
-
-    public static function reserved(): array
+    public static function reserved(Connection $connection): array
     {
-        return array_keys(static::RESERVED_FIELDS);
+        return $connection->getAdapter()->getReservedFields();
     }
 
-    public static function isReserved(string $name): bool
+    public static function isReserved(Connection $connection, string $name): bool
     {
-        return in_array($name, static::reserved());
+        return in_array($name, static::reserved($connection));
     }
 
     public static function normalizeName($name)
@@ -93,31 +39,29 @@ final class Column
         return Str::replace('_', ' ', Str::title($name));
     }
 
-    public static function getFields(Client $connection, string $table): array
+    public static function mapField(array $field): Column
     {
-        return collect(
-            [
-                ...collect(static::RESERVED_FIELDS)
-                    ->map(fn ($field, $key) => [
-                        'column'  => $key,
-                        'type'   => $field['type'],
-                        'notnull' => $field['notnull'] ?? false,
-                        'default' => $field['default'] ?? null,
-                        'autoincrement' => $field['autoincrement'] ?? false,
-                        'comment' => $field['comment'] ?? '',
-                    ])->values(),
-                ...array_map(
-                    fn ($field) => [
-                        'column'  => $field['alias'],
-                        'type'   => $field['type'],
-                        'notnull' => false,
-                        'default' => data_get($field, 'config.default_value.value', null),
-                        'comment' => $field['description'] ?? '',
-                    ],
-                    $connection->get('builder/structures/' . $table . '/fields', true)
-                )
-            ]
-        )
+        return new static([
+            'column'  => $field['alias'],
+            'type'   => $field['type'],
+            'notnull' => false,
+            'default' => data_get($field, 'config.default_value.value', null),
+            'comment' => $field['description'] ?? '',
+        ]);
+    }
+
+    public static function getReservedFields(Connection $connection, string $table): array
+    {
+        return collect(static::reserved($connection))
+            ->map(fn ($field, $key) => [
+                'column'  => $key,
+                'type'   => $field['type'],
+                'notnull' => $field['notnull'] ?? false,
+                'default' => $field['default'] ?? null,
+                'autoincrement' => $field['autoincrement'] ?? false,
+                'comment' => $field['comment'] ?? '',
+            ])
+            ->values()
             ->map(fn ($field) => new static($field))
             ->all();
     }
@@ -131,7 +75,7 @@ final class Column
     {
         return new DoctrineColumn(
             $this->name(),
-            $this->type(),
+            $this->doctrineType(),
             $this->options()
         );
     }
@@ -141,9 +85,14 @@ final class Column
         return $this->column['column'];
     }
 
-    public function type(): Type
+    public function type(): string
     {
-        switch ($this->column['type']) {
+        return $this->column['type'];
+    }
+
+    public function doctrineType(): Type
+    {
+        switch ($this->type()) {
             case 'entry':
                 return new IntegerType;
             case 'checkbox':
