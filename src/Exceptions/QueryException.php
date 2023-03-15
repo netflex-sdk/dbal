3c37e2b4dcc18b43b420ba1d2656a714ac93f747
Thomas Alrek <thomas@alrek.no>
2023-03-15 10:39:19 +0100

    Update QueryException

---
+++ src/Exceptions/QueryException.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Netflex\Database\DBAL\Exceptions;
+
+use Throwable;
+use Illuminate\Database\QueryException as Exception;
+
+class QueryException extends Exception
+{
+    /**
+     * Format the SQL error message.
+     *
+     * @param string $connection
+     * @param string $query
+     * @param array $bindings
+     * @param Throwable $previous
+     * @return string
+     */
+    protected function formatMessage($connection, $query, $bindings, Throwable $previous)
+    {
+        if ($parsed = json_decode($query)) {
+            $command = $parsed->command ?? null;
+
+            if ($request = ($parsed->request ?? null)) {
+                if (isset($request->body) || isset($request->aggs)) {
+                    return $previous->getMessage() . ' (Connection: ' . $connection . ', Query: ' . json_encode($request) . ')';
+                }
+            }
+
+            if ($command) {
+                return $previous->getMessage() . ' (Connection: ' . $connection . ', Command: ' . $command . ')';
+            }
+        }
+
+        return $previous->getMessage();
+    }
+}
